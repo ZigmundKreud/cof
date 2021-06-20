640da55eb1bc2983b716786f59ec4eb4d0f7155b
Christophe Laudon <ch.laudon@gmail.com>
2021-06-20 16:23:41 +0200

    Finalisation des macros
    - Ajout d'une macro "rollHealMacro" qui effectue un jet de soin
    - Ajout d'une macro "rollSkillMacro" qui effectue un jet de compétence (sans attribut obligatoire)
    - Ajout d'une macro "rollDamageMacro" qui effectue un jet de dégât seul (sans item obligatoire)

--- module/system/macros.js
+++ module/system/macros.js
@@ -1,4 +1,5 @@
 import {CofRoll} from "../controllers/roll.js";
+import {CofHealingRoll} from "../controllers/healing-roll.js";
 
 export class Macros {
 
@@ -20,57 +21,62 @@ export class Macros {
     }
 
     static rollStatMacro = async function (actor, stat, bonus = 0, malus = 0, onEnter = "submit", label, description) {
-        if(actor){
-            let statObj;
-            switch(stat){
-                case "for" :
-                case "str" : statObj = eval(`actor.data.data.stats.str`); break;
-                case "dex" : statObj = eval(`actor.data.data.stats.dex`); break;
-                case "con" : statObj = eval(`actor.data.data.stats.con`); break;
-                case "int" : statObj = eval(`actor.data.data.stats.int`); break;
-                case "sag" :
-                case "wis" : statObj = eval(`actor.data.data.stats.wis`); break;
-                case "cha" : statObj = eval(`actor.data.data.stats.cha`); break;
-                case "atc" :
-                case "melee" : statObj = eval(`actor.data.data.attacks.melee`); break;
-                case "atd" :
-                case "ranged" : statObj = eval(`actor.data.data.attacks.ranged`); break;
-                case "atm" :
-                case "magic" : statObj = eval(`actor.data.data.attacks.magic`); break;
-                default :
-                    ui.notifications.error("La compétence à tester n'a pas été reconnue.");
-                    break;
-            }
-            let mod = statObj.mod;
+        // Several tokens selected
+        if (actor === null) return;
+        // No token selected
+        if (actor === undefined) return ui.notifications.error(`${game.i18n.localize("COF.notification.MacroNoTokenSelected")}`);
 
-            // Caractéristiques
-            if (stat === "for" || stat === "str" || stat === "dex") {
-                
-                // Prise en compte de la notion de PJ incompétent
-                if (game.settings.get("cof", "useIncompetentPJ")) {
-                    malus += actor.getIncompetentSkillMalus(stat);
-                }
-
-                // Prise en compte de la notion d'encombrement
-                malus += actor.getOverloadedSkillMalus(stat);
-
-                // Prise en compte des bonus ou malus liés à la caractéristique
-                let skillBonus = statObj.skillbonus;
-                if (skillBonus) bonus += skillBonus;
-                let skillMalus = statObj.skillmalus;
-                if (skillMalus) malus += skillMalus;
-            }
-            await CofRoll.skillRollDialog(actor, label && label.length > 0 ? label : game.i18n.localize(statObj.label), mod, bonus, malus, 20, statObj.superior, onEnter, description);
-        } else {
-            ui.notifications.error("Vous devez sélectionner un token pour pouvoir exécuter cette macro.");
+        let statObj;
+        switch(stat){
+            case "for" :
+            case "str" : statObj = eval(`actor.data.data.stats.str`); break;
+            case "dex" : statObj = eval(`actor.data.data.stats.dex`); break;
+            case "con" : statObj = eval(`actor.data.data.stats.con`); break;
+            case "int" : statObj = eval(`actor.data.data.stats.int`); break;
+            case "sag" :
+            case "wis" : statObj = eval(`actor.data.data.stats.wis`); break;
+            case "cha" : statObj = eval(`actor.data.data.stats.cha`); break;
+            case "atc" :
+            case "melee" : statObj = eval(`actor.data.data.attacks.melee`); break;
+            case "atd" :
+            case "ranged" : statObj = eval(`actor.data.data.attacks.ranged`); break;
+            case "atm" :
+            case "magic" : statObj = eval(`actor.data.data.attacks.magic`); break;
+            default :
+                ui.notifications.error("La compétence à tester n'a pas été reconnue.");
+                break;
         }
+        let mod = statObj.mod;
+
+        // Caractéristiques
+        if (stat === "for" || stat === "str" || stat === "dex") {
+            
+            // Prise en compte de la notion de PJ incompétent
+            if (game.settings.get("cof", "useIncompetentPJ")) {
+                malus += actor.getIncompetentSkillMalus(stat);
+            }
+
+            // Prise en compte de la notion d'encombrement
+            malus += actor.getOverloadedSkillMalus(stat);
+
+            // Prise en compte des bonus ou malus liés à la caractéristique
+            let skillBonus = statObj.skillbonus;
+            if (skillBonus) bonus += skillBonus;
+            let skillMalus = statObj.skillmalus;
+            if (skillMalus) malus += skillMalus;
+        }
+        await CofRoll.skillRollDialog(actor, label && label.length > 0 ? label : game.i18n.localize(statObj.label), mod, bonus, malus, 20, statObj.superior, onEnter, description);
     };
 
     static rollItemMacro = function (itemId, itemName, itemType, bonus = 0, malus = 0, dmgBonus=0, dmgOnly=false, customLabel, skillDescr, dmgDescr) {
         const actor = this.getSpeakersActor();
+        // Several tokens selected
         if (actor === null) return;
+        // No token selected
+        if (actor === undefined) return ui.notifications.error(`${game.i18n.localize("COF.notification.MacroNoTokenSelected")}`);
+
         const item = actor.items.get(itemId);
-        if (!item) return ui.notifications.warn(`${game.i18n.localize("COF.notification.MacroItemMissing")}: "${itemName}"`);
+        if (!item) return ui.notifications.warn(game.i18n.format('COF.notification.MacroItemMissing', {item:itemName}));
         const itemData = item.data;
         if(itemData.data.properties.weapon){
             if(itemData.data.worn){
@@ -98,4 +104,40 @@ export class Macros {
         }
         else { return item.sheet.render(true); }
     };
+
+    static rollHealMacro = function (label, healFormula, isCritical){
+        const actor = this.getSpeakersActor();
+        // Several tokens selected
+        if (actor === null) return;
+        // No token selected
+        if (actor === undefined) return ui.notifications.error(`${game.i18n.localize("COF.notification.MacroNoTokenSelected")}`);
+
+        new CofHealingRoll(label, healFormula, isCritical).roll(actor);
+    }
+
+    static rollSkillMacro = function(label, mod, bonus, malus, critRange, isSuperior = false, description){
+        const actor = this.getSpeakersActor();
+
+        // Several tokens selected
+        if (actor === null) return;
+        // No token selected
+        if (actor === undefined) return ui.notifications.error(`${game.i18n.localize("COF.notification.MacroNoTokenSelected")}`);
+
+        let crit = parseInt(critRange);
+        crit = !isNaN(crit) ? crit : 20;
+        CofRoll.skillRollDialog(actor, label, mod, bonus, malus, crit, isSuperior, "submit", description);  
+    }
+
+    static rollDamageMacro = function(label, dmgFormula, dmgBonus, isCritical, dmgDescr){
+        const actor = this.getSpeakersActor();
+        
+        // Several tokens selected
+        if (actor === null) return;
+        // No token selected
+        if (actor === undefined) return ui.notifications.error(`${game.i18n.localize("COF.notification.MacroNoTokenSelected")}`);
+
+        CofRoll.rollDamageDialog(actor, label, dmgFormula, dmgBonus, isCritical, "submit", dmgDescr);
+          
+    }
+
 }
