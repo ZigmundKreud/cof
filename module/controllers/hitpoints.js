5e47b45cc0208e0e29e79a54db52406fe1306471
Christophe Laudon <ch.laudon@gmail.com>
2021-06-26 09:32:23 +0200

    closes #78 ; Application ou non de la RD pour les dégâts

--- module/controllers/hitpoints.js
+++ module/controllers/hitpoints.js
@@ -1,5 +1,5 @@
 export class Hitpoints {
-    static applyToTargets(amount) {
+    static applyToTargets(amount, dr) {
         // on prend les cibles s'il y en a, sinon on prend les tokens actifs.
         // notation [...] transforme un Set en Array
         let targets = ([...game.user.targets].length > 0) ? [...game.user.targets] : canvas.tokens.objects.children.filter(t => t._controlled);
@@ -9,7 +9,10 @@ export class Hitpoints {
             for(let target of targets){
                 let data = duplicate(target.actor.data);
                 let hp = data.data.attributes.hp;
-                hp.value += amount;
+                // Application de la RD si c'est cochée
+                const finalAmount = amount + (dr ? data.data.attributes.dr.value : 0);
+                hp.value += finalAmount;
+
                 target.actor.update(data);
             }
         }
@@ -19,11 +22,12 @@ export class Hitpoints {
         const btn = $(event.currentTarget);
         const btnType = btn.data("apply");
         const dmg = parseInt(html.find(".dice-total").text());
+        const dr = html.find("#dr").is(":checked");
         switch(btnType){
-            case "full"   : Hitpoints.applyToTargets(-dmg); break;
-            case "half"   : Hitpoints.applyToTargets(-Math.ceil(dmg/2)); break;
-            case "double" : Hitpoints.applyToTargets(-dmg*2); break;
-            case "heal"   : Hitpoints.applyToTargets(dmg); break;
+            case "full"   : Hitpoints.applyToTargets(-dmg, dr); break;
+            case "half"   : Hitpoints.applyToTargets(-Math.ceil(dmg/2), dr); break;
+            case "double" : Hitpoints.applyToTargets(-dmg*2, dr); break;
+            case "heal"   : Hitpoints.applyToTargets(dmg, dr); break;
         }
     }
 }
