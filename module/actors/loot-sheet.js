6e9890298646fd7e18d1cf9052f035d983fc257f
Eryan <shodans@orange.fr>
2021-09-07 21:11:15 +0200

    Ajout prise en compte des objets provenant d'un compendium

--- module/actors/loot-sheet.js
+++ module/actors/loot-sheet.js
@@ -167,6 +167,10 @@ export class CofLootSheet extends CofBaseSheet {
                 if (sameActor) return this._onSortItem(event, itemData);
                 // Create the owned item
                 return this.actor.createEmbeddedDocuments("Item", itemData).then((item)=>{
+                    
+                    // Si il n'y as pas d'actor id, il s'agit d'un objet du compendium, on quitte
+                    if (!data.actorId) return item;
+                                        
                     // Si l'item doit être "move", on le supprime de l'actor précédent
                     let moveItem = game.settings.get("cof","moveItem");                    
                     if (moveItem ^ event.shiftKey) {
